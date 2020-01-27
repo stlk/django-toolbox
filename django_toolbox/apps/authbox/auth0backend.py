6eeed12e61fb70c7376102640ca66e9a16a78e96
Josef Rousek <josef.rousek@gmail.com>
2020-01-27 14:10:24 +0100

    Finish admin auth

--- django_toolbox/apps/authbox/auth0backend.py
+++ django_toolbox/apps/authbox/auth0backend.py
@@ -5,32 +5,37 @@ from social_core.backends.oauth import BaseOAuth2
 
 class Auth0(BaseOAuth2):
     """Auth0 OAuth authentication backend"""
-    name = 'auth0'
-    SCOPE_SEPARATOR = ' '
-    ACCESS_TOKEN_METHOD = 'POST'
+
+    name = "auth0"
+    SCOPE_SEPARATOR = " "
+    ACCESS_TOKEN_METHOD = "POST"
     REDIRECT_STATE = False
     EXTRA_DATA = []
 
     def authorization_url(self):
-        return 'https://' + self.setting('DOMAIN') + '/authorize'
+        return "https://" + self.setting("DOMAIN") + "/authorize"
 
     def access_token_url(self):
-        return 'https://' + self.setting('DOMAIN') + '/oauth/token'
+        return "https://" + self.setting("DOMAIN") + "/oauth/token"
 
     def get_user_id(self, details, response):
         """Return current user id."""
-        return details['user_id']
+        return details["user_id"]
 
     def get_user_details(self, response):
         # Obtain JWT and the keys to validate the signature
-        id_token = response.get('id_token')
-        jwks = request.urlopen('https://' + self.setting('DOMAIN') + '/.well-known/jwks.json')
-        issuer = 'https://' + self.setting('DOMAIN') + '/'
-        audience = self.setting('KEY')  # CLIENT_ID
-        payload = jwt.decode(id_token, jwks.read(), algorithms=['RS256'], audience=audience, issuer=issuer)
+        id_token = response.get("id_token")
+        jwks = request.urlopen(
+            "https://" + self.setting("DOMAIN") + "/.well-known/jwks.json"
+        )
+        issuer = "https://" + self.setting("DOMAIN") + "/"
+        audience = self.setting("KEY")  # CLIENT_ID
+        payload = jwt.decode(
+            id_token,
+            jwks.read(),
+            algorithms=["RS256"],
+            audience=audience,
+            issuer=issuer,
+        )
 
-        return {'username': payload['nickname'],
-                'first_name': payload['name'],
-                'picture': payload['picture'],
-                'user_id': payload['sub'],
-                }
+        return {"username": payload["name"], "user_id": payload["sub"]}
--- django_toolbox/apps/authbox/urls.py
+++ django_toolbox/apps/authbox/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "authbox"
 
 urlpatterns = [
-    path('logout', views.logout),
-    path('', include('social_django.urls', namespace="social")),
+    path("logout", views.logout, name="logout"),
+    path("new-user", views.new_user),
+    path("", include("social_django.urls", namespace="social")),
 ]
