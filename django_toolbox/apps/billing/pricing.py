2b2255fc4d308ba46136dcd24dd1056aab9c5ce0
tystar <stepankalucinova@gmail.com>
2020-06-30 14:44:22 +0200

    Add annual subscription

--- django_toolbox/apps/billing/pricing.py
+++ django_toolbox/apps/billing/pricing.py
@@ -1,13 +1,42 @@
-import shopify
+import requests
+
 from django.conf import settings
 from django.shortcuts import reverse
+from django_toolbox.shopify_graphql import _check_for_errors
+import shopify
 
+
+ANNUAL_CHARGE_MUTATION = '''mutation AnnualSubscriptionCreate($name: String!, $return_url: URL!, $trial_days: Int!, $amount: Decimal!){
+    appSubscriptionCreate(
+        name: $name
+        returnUrl: $return_url
+        trialDays: $trial_days
+        test:true
+        lineItems: [
+        {
+            plan: {
+                appRecurringPricingDetails: {
+                    price: { amount: $amount, currencyCode: USD }
+                    interval: ANNUAL
+                }
+            }
+        }
+        ]
+    ) {
+        appSubscription {
+            id
+        }
+        confirmationUrl
+        userErrors {
+            field
+            message
+        }
+    }
+}
+'''
 
 def create_charge(request, shop):
     price, trial_days, name = settings.BILLING_FUNCTION(shop)
-    if request.session.get("promo_code") == "carson":
-        name = f"{name} - Carson Promo"
-        price = price * 0.75
 
     return shopify.RecurringApplicationCharge.create(
         {
@@ -20,3 +49,22 @@ def create_charge(request, shop):
             "test": settings.SHOPIFY_APP_TEST_CHARGE,
         }
     )
+
+
+def create_annual_charge(request, shop):
+    price, trial_days, name = settings.ANNUAL_BILLING_FUNCTION(shop)
+    return_url = request.build_absolute_uri(reverse("billing:activate-charge"))
+    headers = {
+        "X-Shopify-Access-Token": shop.token,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+    url = f"https://{shop.myshopify_domain}/admin/api/unstable/graphql.json"
+    variables = {"name": name, "return_url": return_url, "trial_days": trial_days, "amount": price}
+    data = {"query": ANNUAL_CHARGE_MUTATION, "variables": variables}
+    response = requests.post(url, headers=headers, json=data, timeout=10)
+    response.raise_for_status()
+    content = response.json()
+    _check_for_errors(content)
+
+    return content["data"]["appSubscriptionCreate"]["confirmationUrl"]
