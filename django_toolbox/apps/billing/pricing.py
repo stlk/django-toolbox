208afdabed38796024ea33ee061793f258f98c4d
tystar <stepankalucinova@gmail.com>
2020-07-16 14:37:02 +0200

    Get subscription data & Add api version parameter for run_query()

--- django_toolbox/apps/billing/pricing.py
+++ django_toolbox/apps/billing/pricing.py
@@ -1,17 +1,14 @@
-import requests
-
 from django.conf import settings
 from django.shortcuts import reverse
-from django_toolbox.shopify_graphql import _check_for_errors
+from django_toolbox.shopify_graphql import _check_for_errors, run_query
 import shopify
 
 
-ANNUAL_CHARGE_MUTATION = '''mutation AnnualSubscriptionCreate($name: String!, $return_url: URL!, $trial_days: Int!, $amount: Decimal!){
+ANNUAL_CHARGE_MUTATION = """mutation AnnualSubscriptionCreate($name: String!, $return_url: URL!, $trial_days: Int!, $amount: Decimal!){
     appSubscriptionCreate(
         name: $name
         returnUrl: $return_url
         trialDays: $trial_days
-        test:true
         lineItems: [
         {
             plan: {
@@ -33,10 +30,47 @@ ANNUAL_CHARGE_MUTATION = '''mutation AnnualSubscriptionCreate($name: String!, $r
         }
     }
 }
-'''
+"""
+
+SUBSCRIPTION_QUERY = """
+{
+  appInstallation {
+    activeSubscriptions {
+      name
+      status
+      currentPeriodEnd
+      lineItems {
+        plan {
+          pricingDetails {
+            ... on AppRecurringPricing {
+              interval
+              price {
+                amount
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def get_subscription(shop):
+    content = run_query(
+        shop.token, shop.myshopify_domain, SUBSCRIPTION_QUERY, api_version="2020-07",
+    )
+    _check_for_errors(content)
+    if content["data"]["appInstallation"]["activeSubscriptions"]:
+        return content["data"]["appInstallation"]["activeSubscriptions"][0]
+
 
 def create_charge(request, shop):
     price, trial_days, name = settings.BILLING_FUNCTION(shop)
+    if request.session.get("promo_code") == "carson":
+        name = f"{name} - Carson Promo"
+        price = price * 0.75
 
     return shopify.RecurringApplicationCharge.create(
         {
@@ -52,19 +86,28 @@ def create_charge(request, shop):
 
 
 def create_annual_charge(request, shop):
-    price, trial_days, name = settings.ANNUAL_BILLING_FUNCTION(shop)
-    return_url = request.build_absolute_uri(reverse("billing:activate-charge"))
-    headers = {
-        "X-Shopify-Access-Token": shop.token,
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    }
-    url = f"https://{shop.myshopify_domain}/admin/api/unstable/graphql.json"
-    variables = {"name": name, "return_url": return_url, "trial_days": trial_days, "amount": price}
-    data = {"query": ANNUAL_CHARGE_MUTATION, "variables": variables}
-    response = requests.post(url, headers=headers, json=data, timeout=10)
-    response.raise_for_status()
-    content = response.json()
-    _check_for_errors(content)
+    subscription_node = get_subscription(shop)
 
-    return content["data"]["appSubscriptionCreate"]["confirmationUrl"]
+    if subscription_node:
+        price = subscription_node["lineItems"][0]["plan"]["pricingDetails"]["price"][
+            "amount"
+        ]
+        trial_days = subscription_node["trialDays"]
+        name = subscription_node["name"]
+        return_url = request.build_absolute_uri(reverse("billing:activate-charge"))
+        content = run_query(
+            shop.token,
+            shop.myshopify_domain,
+            ANNUAL_CHARGE_MUTATION,
+            variables={
+                "name": f"{name} - Annual",
+                "return_url": return_url,
+                "trial_days": trial_days,
+                "amount": price,
+            },
+            api_version="2020-07",
+        )
+
+        _check_for_errors(content)
+
+        return content["data"]["appSubscriptionCreate"]["confirmationUrl"]
