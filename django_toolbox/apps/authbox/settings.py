51b6e81fd10ee97357b1364026beb37ff6ce476d
tystar <stepankalucinova@gmail.com>
2020-11-20 10:33:21 +0100

    Use google auth with django_social

--- django_toolbox/apps/authbox/settings.py
+++ django_toolbox/apps/authbox/settings.py
@@ -2,15 +2,29 @@ import os
 
 # Django-toolbox auth settings
 SOCIAL_AUTH_TRAILING_SLASH = False
-SOCIAL_AUTH_AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
-SOCIAL_AUTH_AUTH0_KEY = os.environ.get("AUTH0_KEY")
-SOCIAL_AUTH_AUTH0_SECRET = os.environ.get("AUTH0_SECRET")
-SOCIAL_AUTH_AUTH0_SCOPE = ["openid", "profile"]
 SOCIAL_AUTH_URL_NAMESPACE = "auth:social"
 SOCIAL_AUTH_USER_FIELDS = ["username"]
+SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
+SOCIAL_AUTH_USE_UNIQUE_USER_ID = False
 
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/admin"
 
 SOCIAL_AUTH_NEW_USER_REDIRECT_URL = SOCIAL_AUTH_LOGIN_REDIRECT_URL
 SOCIAL_AUTH_NEW_ASSOCIATION_REDIRECT_URL = SOCIAL_AUTH_LOGIN_REDIRECT_URL
 SOCIAL_AUTH_INACTIVE_USER_URL = SOCIAL_AUTH_LOGIN_REDIRECT_URL
+
+# Google Auth0
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
+
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    # 'social_core.pipeline.user.get_username',
+    # 'social_core.pipeline.social_auth.associate_by_email',
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
--- django_toolbox/apps/authbox/views.py
+++ django_toolbox/apps/authbox/views.py
@@ -1,8 +1,6 @@
-from urllib.parse import urlencode
-
-from django.conf import settings
 from django.contrib.auth import logout as log_out
 from django.http import HttpResponseRedirect
+
 from social_django.views import disconnect as social_disconnect
 
 
@@ -14,13 +12,7 @@ def disconnect(request):
 
 def logout(request):
     log_out(request)
-    return_to = urlencode({"returnTo": request.build_absolute_uri("/")})
-    logout_url = "https://%s/v2/logout?client_id=%s&%s" % (
-        settings.SOCIAL_AUTH_AUTH0_DOMAIN,
-        settings.SOCIAL_AUTH_AUTH0_KEY,
-        return_to,
-    )
-    return HttpResponseRedirect(logout_url)
+    return HttpResponseRedirect("/admin")
 
 
 def override_login(request):
